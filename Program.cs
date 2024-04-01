a8b0d4935e7fcd2bde3bbbcafb6f45e0f6b7ee30
Yuliya <Yulya.savko.97@gmail.com>
2024-04-02 00:35:23 +0200

    Добавлено решение задачи

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] FilterArray(string[] array, int k){
+   int count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+    if (array[i].Length <= k)
+    {
+        count++;
+    }
+   }
+   int n = 0;
+   string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= k)
+        {
+            result[n] = array[i];
+            n++;
+        }
+    }
+    return result;
+}
+
+string[] arr = {"Hello", "2", "world", ":-)"};
+string[] result = FilterArray(arr, 3);
+
+Console.WriteLine($"[{string.Join(", ", result)}]");
